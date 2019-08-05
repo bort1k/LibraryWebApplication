46fba4347e646251d2ef4bff1824d76cbb1085a8
bort1k <nikita.bortnichuk41@gmail.com>
2019-08-05 14:39:48 +0300

    add tests

---
+++ src/test/java/AuthorTest.java
@@ -0,0 +1,14 @@
+import com.bortni.model.dao.AuthorDao;
+import com.bortni.model.dao.DaoFactory;
+import com.bortni.model.dao.implementation.DaoFactoryImpl;
+import org.junit.Test;
+
+public class AuthorTest {
+    private DaoFactory daoFactory = new DaoFactoryImpl();
+    private AuthorDao authorDao = daoFactory.createAuthorDao();
+
+    @Test
+    public void getAll(){
+        System.out.println(authorDao.getAll());
+    }
+}
---
+++ src/test/java/BookTest.java
@@ -0,0 +1,23 @@
+import com.bortni.model.dao.BookDao;
+import com.bortni.model.dao.DaoFactory;
+import com.bortni.model.dao.implementation.DaoFactoryImpl;
+import com.bortni.model.entities.Book;
+import org.junit.Test;
+
+import java.util.List;
+
+public class BookTest {
+    private DaoFactory daoFactory = new DaoFactoryImpl();
+    private BookDao bookDao = daoFactory.createBookDao();
+
+    @Test
+    public void getAllAvail(){
+        System.out.println(bookDao.getAllAvailable());
+    }
+
+    @Test
+    public void getById(){
+        int id = 2;
+        System.out.println(bookDao.getById(id));
+    }
+}
